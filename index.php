cb4f03122e761b3e2e43109e428451196870295d
Luis Fernando Rosales Serrano <l23240044@smartin.tecnm.mx>
2026-03-10 15:42:34 -0600

    index y reporets corregidos

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
     <title>Document</title>
     <main> pagina principal 
         <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
         <a href="reportes.php">Ver Reportes</a>
+        <br>
         <a href="dashboard.php">Ver Dashboard</a>
             
     </main>
