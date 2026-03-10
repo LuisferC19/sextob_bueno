80b2f1f6f53823b376999b71b0c9d06dee5bfe63
Luis Fernando Rosales Serrano <l23240044@smartin.tecnm.mx>
2026-03-10 15:34:52 -0600

    proyecto inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <main> pagina principal 
+        <br>
+        <a href="reportes.php">Ver Reportes</a>
+        <a href="dashboard.php">Ver Dashboard</a>
+            
+    </main>
+
+</head>
+<body>
+    
+</body>
+</html>
